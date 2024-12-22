bb13d408fabba3d4a61f86f12cceaa7e1ae5dc07
iTomGeller <303327065@qq.com>
2024-12-23 00:02:11 +0800

    add mapper for mybatis frame

--- mooc-content/mooc-content-model/src/main/java/com/mooc/content/model/dto/QueryCourseParamsDto.java
+++ mooc-content/mooc-content-model/src/main/java/com/mooc/content/model/dto/QueryCourseParamsDto.java
@@ -1,5 +1,7 @@
 package com.mooc.content.model.dto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
 
@@ -8,10 +10,15 @@ import lombok.ToString;
 public class QueryCourseParamsDto {
 
     //审核状态
+    @ApiModelProperty("审核状态")
     private String auditStatus;
+
     //课程名称
+    @ApiModelProperty("课程名称")
     private String courseName;
+
     //发布状态
+    @ApiModelProperty("发布状态")
     private String publishStatus;
 
 }
