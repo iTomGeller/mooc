5d227c2a0e7015c09e148b76b22c8f3304d6c342
iTomGeller <303327065@qq.com>
2024-12-22 23:57:56 +0800

    add api annotations for methods

--- mooc-base/src/main/java/com/mooc/base/model/PageParams.java
+++ mooc-base/src/main/java/com/mooc/base/model/PageParams.java
@@ -1,5 +1,6 @@
 package com.mooc.base.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
 
@@ -9,13 +10,19 @@ import lombok.ToString;
 public class PageParams {
 
     //当前页码
+    @ApiModelProperty("当前页码")
     private Long pageNo = 1l;
 
     //每页显示记录数
+    @ApiModelProperty("每页显示记录数")
     private Long pageSize = 1l;
 
     public PageParams(Long pageNo, Long pageSize) {
         this.pageNo = pageNo;
         this.pageSize = pageSize;
+    }
+
+    public PageParams() {
+
     }
 }
--- mooc-content/mooc-content-api/src/main/java/com/mooc/content/CourseBaseInfoController.java
+++ mooc-content/mooc-content-api/src/main/java/com/mooc/content/CourseBaseInfoController.java
@@ -4,14 +4,20 @@ import com.mooc.base.model.PageParams;
 import com.mooc.base.model.PageResult;
 import com.mooc.content.model.dto.QueryCourseParamsDto;
 import com.mooc.content.model.po.CourseBase;
-import org.springframework.web.bind.annotation.RequestMapping;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "课程信息管理接口", tags = "课程信息管理接口")
 @RestController
+//避免所有返回都加一个response-body
 public class CourseBaseInfoController {
 
-    @RequestMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto) {
+    @ApiOperation("课程查询接口")
+    @PostMapping("/course/list")
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto) {
         return null;
     }
 }
