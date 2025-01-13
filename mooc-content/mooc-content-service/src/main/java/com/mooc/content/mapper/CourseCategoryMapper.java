4249274344fac400cb1a782d65209523a0f768d9
iTomGeller <303327065@qq.com>
2025-01-13 17:20:06 +0800

    update xml settings

--- mooc-content/mooc-content-service/src/main/java/com/mooc/content/mapper/CourseCategoryMapper.java
+++ mooc-content/mooc-content-service/src/main/java/com/mooc/content/mapper/CourseCategoryMapper.java
@@ -1,8 +1,11 @@
 package com.mooc.content.mapper;
 
 import com.mooc.content.model.po.CourseCategory;
+import com.mooc.content.model.dto.CourseCategoryTreeDto;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,8 @@ import org.springframework.stereotype.Repository;
  */
 public interface CourseCategoryMapper extends BaseMapper<CourseCategory> {
 
+    //使用递归查询分类
+    public List<CourseCategoryTreeDto> selectTreeNodes(String id);
+
 }
+
