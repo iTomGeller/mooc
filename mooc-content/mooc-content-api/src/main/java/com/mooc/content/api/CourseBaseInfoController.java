811d0c9e3e49e46da5ea561180c1a797eaf78e42
iTomGeller <303327065@qq.com>
2025-01-13 16:45:24 +0800

    add CourseBaseInfoController at api level

---
+++ mooc-content/mooc-content-api/src/main/java/com/mooc/content/api/CourseBaseInfoController.java
@@ -0,0 +1,49 @@
+package com.mooc.content.api;
+
+import com.mooc.base.model.PageParams;
+import com.mooc.base.model.PageResult;
+import com.mooc.content.model.dto.AddCourseDto;
+import com.mooc.content.model.dto.CourseBaseInfoDto;
+import com.mooc.content.model.dto.QueryCourseParamsDto;
+import com.mooc.content.model.po.CourseBase;
+import com.mooc.content.service.CourseBaseInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description TODO
+ * @date 2023/2/11 15:44
+ */
+@Api(value = "课程信息管理接口",tags = "课程信息管理接口")
+@RestController
+public class CourseBaseInfoController {
+
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
+    @ApiOperation("课程查询接口")
+    @PostMapping("/course/list")
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required=false) QueryCourseParamsDto queryCourseParamsDto) {
+
+        PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDto);
+
+        return courseBasePageResult;
+
+    }
+    @ApiOperation("新增课程")
+    @PostMapping("/course")
+    public CourseBaseInfoDto createCourseBase(@RequestBody AddCourseDto addCourseDto){
+        //获取到用户所属机构的id
+        Long companyId = 1232141425L;
+//        int i = 1/0;
+        CourseBaseInfoDto courseBase = courseBaseInfoService.createCourseBase(companyId, addCourseDto);
+        return courseBase;
+    }
+
+}
