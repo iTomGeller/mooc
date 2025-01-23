0c5af36eb641e56fa4440400fee2f96a296c7546
iTomGeller <303327065@qq.com>
2025-01-23 15:04:42 +0800

    Update DictionaryController.java

--- mooc-system/mooc-system-api/src/main/java/com/mooc/system/controller/DictionaryController.java
+++ mooc-system/mooc-system-api/src/main/java/com/mooc/system/controller/DictionaryController.java
@@ -4,6 +4,7 @@ import com.mooc.system.model.po.Dictionary;
 import com.mooc.system.service.DictionaryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
