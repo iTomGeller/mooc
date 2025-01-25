0d582eb7c62f7cd608484e819def49639e616c09
iTomGeller <303327065@qq.com>
2025-01-25 18:31:36 +0800

    add mooc-media directory

---
+++ mooc-media/mooc-media-model/src/main/java/com/mooc/media/model/dto/UploadFileResultDto.java
@@ -0,0 +1,17 @@
+package com.mooc.media.model.dto;
+
+import com.mooc.media.model.po.MediaFiles;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description TODO
+ * @date 2023/2/17 15:06
+ */
+@Data
+@ToString
+public class UploadFileResultDto extends MediaFiles {
+
+}
---
+++ mooc-media/mooc-media-service/src/main/java/com/mooc/media/mapper/MqMessageHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.mooc.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mooc.media.model.po.MqMessageHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageHistoryMapper extends BaseMapper<MqMessageHistory> {
+
+}
