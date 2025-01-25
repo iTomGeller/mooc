887bcd58b0f54edf5d0288244200e567702c7284
iTomGeller <303327065@qq.com>
2025-01-25 18:27:43 +0800

    add mooc-gateway directory

---
+++ mooc-gateway/src/test/java/com/mooc/MoocGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooc;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class moocPlusGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
