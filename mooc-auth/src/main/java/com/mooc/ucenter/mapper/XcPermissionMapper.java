f4e4ce0b3a8f8bd4e5214b87e9be707c247d6c9f
iTomGeller <303327065@qq.com>
2025-02-03 21:07:45 +0800

    add mooc-auth directory

---
+++ mooc-auth/src/main/java/com/mooc/ucenter/mapper/XcPermissionMapper.java
@@ -0,0 +1,15 @@
+package com.mooc.ucenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mooc.ucenter.model.po.XcPermission;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcPermissionMapper extends BaseMapper<XcPermission> {
+
+}
---
+++ mooc-auth/src/main/java/com/mooc/ucenter/mapper/XcRoleMapper.java
@@ -0,0 +1,15 @@
+package com.mooc.ucenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mooc.ucenter.model.po.XcRole;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcRoleMapper extends BaseMapper<XcRole> {
+
+}
