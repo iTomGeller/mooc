404ff2e086b8bdfd613f8fb734300915e425661f
iTomGeller <303327065@qq.com>
2025-02-03 21:09:44 +0800

    add mooc-orders directory

---
+++ mooc-orders/mooc-orders-service/src/main/java/com/mooc/orders/mapper/XcPayRecordMapper.java
@@ -0,0 +1,15 @@
+package com.mooc.orders.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mooc.orders.model.po.XcPayRecord;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcPayRecordMapper extends BaseMapper<XcPayRecord> {
+
+}
