b8a393b41318e10a33b81280ebcf1c8d303d1c3c
iTomGeller <303327065@qq.com>
2025-02-03 21:10:49 +0800

    Update ContentApplication.java

--- mooc-content/mooc-content-api/src/main/java/com/mooc/ContentApplication.java
+++ mooc-content/mooc-content-api/src/main/java/com/mooc/ContentApplication.java
@@ -3,7 +3,10 @@ package com.mooc;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableFeignClients(basePackages={"com.mooc.content.feignclient"})
 @EnableSwagger2Doc
 @SpringBootApplication
 public class ContentApplication {
